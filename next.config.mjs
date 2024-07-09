7a06614795bed155e7c4f89301e507322dda86e2
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-09 20:21:18 +0900

    깃헙연동

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  assetPrefix: isProd ? '/hy-ai-erica.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
